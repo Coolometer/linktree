ca791048a062512ed2849934211d85686bddd974
Coolometer <doctor.frankenschwein@gmail.com>
2026-04-07 14:31:09 +0200

    Update fancy.js

--- fancy.js
+++ fancy.js
@@ -1,6 +1,7 @@
 function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.add("show");
+  console.log("Show");
   navigator.clipboard.writeText("nona+spam@oh-nona.eu");
   setTimeout(function() {
   popup.classList.remove("show");
