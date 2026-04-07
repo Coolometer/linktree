412c75353e440de8847d271279a3ca9b01ecec45
Coolometer <doctor.frankenschwein@gmail.com>
2026-04-07 14:13:08 +0200

    Add actual copy to clipboard function

--- fancy.js
+++ fancy.js
@@ -1,7 +1,7 @@
 function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.add("show");
-  console.log("toggle")
+  navigator.clipboard.writeText("nona+spam@oh-nona.eu");
   setTimeout(function() {
   popup.classList.remove("show");
 }, 2000);
