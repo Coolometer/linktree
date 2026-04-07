8f0582b5811584485a49568e547a494e691f9659
Coolometer <doctor.frankenschwein@gmail.com>
2026-04-07 22:19:14 +0200

    I hope this allows the clipboard thingy for general buttons

--- fancy.js
+++ fancy.js
@@ -1,11 +1,14 @@
 function myFunction() {
   var popup = document.getElementById("myPopup");
+  var text = this.querySelector(".info")[0];
   popup.classList.add("show");
-  navigator.clipboard.writeText("nona+spam@oh-nona.eu");
+  navigator.clipboard.writeText(text);
   setTimeout(function() {
   popup.classList.remove("show");
 }, 2000);
 }
 window.onload = function () {
-  document.getElementById("clickable").addEventListener("click", myFunction);
+  var elements = document.getElementsByClass("clickable");
+  elements.foreach((element)=>element.addEventListener("click", myFunction););
+  
 };
