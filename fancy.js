b44615b3dd8eaf8230011f37161cd55f1c3ce958
Coolometer <doctor.frankenschwein@gmail.com>
2026-04-07 14:46:38 +0200

    Idiot forgot to add the event

--- fancy.js
+++ fancy.js
@@ -1,9 +1,10 @@
 function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.add("show");
-  console.log("Show");
   navigator.clipboard.writeText("nona+spam@oh-nona.eu");
   setTimeout(function() {
   popup.classList.remove("show");
 }, 2000);
 }
+
+document.onload(document.getElementById("clickable").addEventListener("click", myFunction));
